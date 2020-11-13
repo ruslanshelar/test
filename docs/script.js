c16819280265728ad8e189261fbf77d316455bbd
shredderr <32563167+ruslanshelar@users.noreply.github.com>
2020-11-13 14:41:20 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,9 @@
 
+        var a = document.createElement('a');
+        var linkText = document.createTextNode("from external script");
+        var firstPartHref = "http://www."
+        var secondPartHref = "linkio.com"
+        a.appendChild(linkText);
+        a.title = "my title text";
+        a.href = firstPartHref + secondPartHref
+        document.body.appendChild(a);    
